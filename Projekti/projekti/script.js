91a2d0d754b8a704e202e55f6d64888f1f56c9e7
vanchess-git <72012822+vanchess-git@users.noreply.github.com>
2020-09-30 03:33:28 +0300

    Update index.html
    HSL markerit

--- Projekti/projekti/script.js
+++ Projekti/projekti/script.js
@@ -57,3 +57,36 @@ fetch(proxyOsoite + myHelsinkiOsoite).then((vastaus) => {
 }).catch(function(error){console.log(error);
 })}
 myHelsinki();
+/*
+//VILI
+//tämä on siirretty kommenttiin, jotta se ei tuo liikaa dataa muun koodin testailuun
+//haetaan HSL myyntipisteet
+function VHSLlipunmyynti() {
+ const VHSLlipunmyyntiADD = 'https://opendata.arcgis.com/datasets/727c6618a0814f8ba21bb00c9cb34019_0.geojson';
+ fetch(VHSLlipunmyyntiADD)
+     .then(function (Vvastaus) {
+       return Vvastaus.json();
+  }).then(function (VHSLlipunmyyntispot){
+    console.log(VHSLlipunmyyntispot);
+
+//markerien teko HSL myyntipisteille alkaa tästä
+    for (let Vi = 0; Vi < VHSLlipunmyyntispot.features.length; Vi++) {
+      let Vlong = VHSLlipunmyyntispot.features[Vi].geometry.coordinates[1];
+      let Vlat = VHSLlipunmyyntispot.features[Vi].geometry.coordinates[0];
+      console.log(Vlat + ' Lat - ' + Vlong + ' Lon');
+      //Lisää avustavan tekstin popupiin, jos se ei ole null
+      let Vaddhelp = '';
+      if (VHSLlipunmyyntispot.features[Vi].properties.Address_help_fi !== null){(Vaddhelp = VHSLlipunmyyntispot.features[Vi].properties.Address_help_fi)} else {Vaddhelp = ''}
+      //Lisää vyöhykkeen A-D popupiin, jos se ei ole null
+      let Vzone = '';
+      if (VHSLlipunmyyntispot.features[Vi].properties.Address_help_fi !== null){(Vzone = VHSLlipunmyyntispot.features[Vi].properties.Zone)} else {Vzone = ''}
+      //lisää markerin kartalle ja antaa sille popupiin tietoja
+      L.marker([Vlong, Vlat]).addTo(kartta).bindPopup(VHSLlipunmyyntispot.features[Vi].properties.Tyyppi + '</br>' + VHSLlipunmyyntispot.features[Vi].properties.Address_fi + '</br>' + VHSLlipunmyyntispot.features[Vi].properties.City_fi + ' Vyöhyke: ' + Vzone + '</br>' + Vaddhelp)
+    }
+//tarvitaan lisää värivaihtoehtoja markereille tunnistusta varten
+ }).catch(function(error){console.log(error);
+})}
+VHSLlipunmyynti();
+
+//VILI
+*/
