95b847481303a8819d5d8a547e231f0f8b5d2570
TeemuTallskog <72005659+TeemuTallskog@users.noreply.github.com>
2020-10-08 19:34:12 +0300

    patch

--- Projekti/projekti/script.js
+++ Projekti/projekti/script.js
@@ -371,11 +371,12 @@ paivitysNappiT.addEventListener('click', function(){
 //---------Sidebad------------------
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  //document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";}
+  //document.getElementById("main").style.marginLeft= "0";
+}
 
 
